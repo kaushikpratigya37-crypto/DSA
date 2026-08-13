65bbc9bd852d358c77dfce9f42d54a8c62dd95b2
Pratigya Kaushik <kaushikpratigya37@gmail.com>
2026-08-13 15:57:05 +0530

    min and max array elements

---
+++ basics/7.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    printf("Hello world!\n");
+    int a=20;
+    int arr[5]={1,2,3,4,5};
+    for(int i=1;i<5;i++){
+        printf("%d", i);
+    }
+    return 0;
+}
